15b02f2aa43ace91be52daa19a5a6ce8ee5c0308
Ales Bacul <alesbacul@gmail.com>
2023-02-21 20:47:02 +0100

    JS vars, CSS classes renamed, code cleaned

--- script.js
+++ script.js
@@ -9,75 +9,70 @@ let notesArr = [];
 // DOM HTML selectors
 const btnSubmit = document.getElementById("btn-submit");
 const taskList = document.getElementById("tasklist");
-const btnSave = document.getElementById("btn-to-ls");
-const inputNote = document.getElementById("input-note");
-const form = document.getElementById("to-do-form");
+const inputTask = document.getElementById("input-task");
+const formTask = document.getElementById("form-task");
 // Form listener
-form.addEventListener("submit", function (e) {  
+formTask.addEventListener("submit", function (e) {  
   e.preventDefault();
-  if(inputNote.value.length > 1000) {alert("Překročili jste limit 1 000 znaků")} 
+  if(inputTask.value.length > 1000) {alert("Překročili jste limit 1 000 znaků")} 
   else{
-  let taskValue = inputNote.value;
+  let taskValue = inputTask.value;
   
   let newId = uuidv4();
   notesArr.push({ id: newId, task: taskValue });
   // Pin to our board
   renderTasks({ id: newId, task: taskValue });
   // Clear the input
-  inputNote.value = "";
+  inputTask.value = "";
   // Pass notes to LocalStorage
   localStorage.setItem("storage", JSON.stringify(notesArr));}
 });
 
 
-
 function renderTasks(input) {
-  
+  // BS col
   let col = document.createElement("div");
-   
   col.className = "col-12 col-sm-6 col-md-3";
+  // BS card
   let card = document.createElement("div");
   card.className = "card mb-3 mt-3";
+  // BS card body
   let cardBody = document.createElement("div");
   cardBody.className = "card-body";
+  // BS card text
   let cardText = document.createElement("p");
   cardText.className = "card-text";
-  let delBtn = document.createElement("a");
-  delBtn.className = "btn btn-danger";
- 
+  // BS button
+  let btnDel = document.createElement("a");
+  btnDel.className = "btn btn-danger";
+  // Setting balues
+  cardText.innerHTML = input.task;
+  btnDel.innerHTML = "Odstranit";
+  // Appending
   col.appendChild(card);
   card.appendChild(cardBody);
   cardBody.appendChild(cardText);
-  cardBody.appendChild(delBtn);
-
-  cardText.innerHTML = input.task;
-  delBtn.innerHTML = "Odstranit";
-   
-  
+  cardBody.appendChild(btnDel);
+  // Appending to element in INDEX.html
   taskList.appendChild(col);
-  taskList.className = "row ";
+  taskList.className = "row";
   initMasonry();
-  delBtn.addEventListener("click", function (e) {
+  btnDel.addEventListener("click", function (e) {
     deleteNote(input.id);
     removeAllChildNodes(taskList);
     notesArr.forEach((element) => {
       renderTasks(element);
     });
-
     localStorage.setItem("storage", JSON.stringify(notesArr));
   });
-
-
 }
-
+// Deletes from notesArr by ID
 function deleteNote(id) {
-  // delete from notesArr by ID
   let result = notesArr.filter((element) => element.id != id);
-  // update notesArr
   notesArr = result;
-  console.log(notesArr);
+  
 }
-
+// If there are tasks in LS, render them
 window.addEventListener("load", function () {
   if (localStorage.getItem("storage") != null) {
     let fromLS = JSON.parse(localStorage.getItem("storage"));
@@ -90,7 +85,7 @@ window.addEventListener("load", function () {
     console.log("***");
   }
 });
-
+// Masonry init
 function initMasonry() {
   var container = document.querySelector('#tasklist');
   new Masonry(container, {
@@ -98,6 +93,7 @@ function initMasonry() {
     percentPosition: true
   });
 }
+// Onload
 window.addEventListener('load', function() {
   initMasonry();
 });
