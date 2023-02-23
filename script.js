086f256c9a2d89aa717d756f3d785fd54f4698dc
Ales Bacul <alesbacul@gmail.com>
2023-02-23 18:49:15 +0100

    2 buttons (delete/change color), script and design updates

--- script.js
+++ script.js
@@ -1,34 +1,17 @@
+const colors = ["#ffffff", "#bFFFbF", "#ffffbf"];
+// Functions
+function initMasonry() {
+  var container = document.querySelector("#div-tasks");
+  new Masonry(container, {
+    itemSelector: ".col-12",
+    percentPosition: true,
+  });
+}
 function removeAllChildNodes(parent) {
   while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
   }
 }
- 
-// Tasklist
-let tasksArray = [];
-// DOM HTML selectors
-const btnSubmit = document.getElementById("btn-submit");
-const divTasks = document.getElementById("div-tasks");
-const inputTask = document.getElementById("input-task");
-const formTask = document.getElementById("form-task");
-// Form listener
-formTask.addEventListener("submit", function (e) {  
-  e.preventDefault();
-  if(inputTask.value.length > 1000) {alert("Překročili jste limit 1 000 znaků")} 
-  else{
-  let taskValue = inputTask.value;
-  
-  let newId = uuidv4();
-  tasksArray.push({ id: newId, task: taskValue });
-  // Pin to our board
-  renderTasks({ id: newId, task: taskValue });
-  // Clear the input
-  inputTask.value = "";
-  // Pass notes to LocalStorage
-  localStorage.setItem("storage", JSON.stringify(tasksArray));}
-});
-
-
 function renderTasks(input) {
   // BS col
   let col = document.createElement("div");
@@ -39,20 +22,39 @@ function renderTasks(input) {
   // BS card body
   let cardBody = document.createElement("div");
   cardBody.className = "card-body";
+
+  card.style.backgroundColor = `${input.color}`;
   // BS card text
   let cardText = document.createElement("p");
   cardText.className = "card-text";
+  // BS btn group
+  let btnGroup = document.createElement("div");
+  btnGroup.className = "btn-group";
   // BS button
   let btnDel = document.createElement("a");
-  btnDel.className = "btn btn-danger";
+  btnDel.className = "btn btn-danger btn-sm";
+  let iconDel = document.createElement("i");
+  iconDel.className = "bi bi-x-square-fill";
+  // Color btn
+  let btnCol = document.createElement("a");
+  btnCol.className = "btn btn-primary btn-sm";
+  btnCol.id = input.id;
+  let iconCol = document.createElement("i");
+  iconCol.className = "bi bi-palette-fill";
   // Setting balues
   cardText.innerHTML = input.task;
-  btnDel.innerHTML = "Odstranit";
+
   // Appending
   col.appendChild(card);
   card.appendChild(cardBody);
   cardBody.appendChild(cardText);
-  cardBody.appendChild(btnDel);
+
+  cardBody.appendChild(btnGroup);
+  btnGroup.appendChild(btnDel);
+  btnDel.appendChild(iconDel);
+  btnCol.appendChild(iconCol);
+  btnGroup.appendChild(btnCol);
+
   // Appending to element in INDEX.html
   divTasks.appendChild(col);
   divTasks.className = "row";
@@ -65,35 +67,74 @@ function renderTasks(input) {
     });
     localStorage.setItem("storage", JSON.stringify(tasksArray));
   });
+
+  btnCol.addEventListener("click", function (e) {
+    console.log(e.target.id);
+
+    for (let i = 0; i < tasksArray.length; i++) {
+      if (tasksArray[i].id === e.target.id) {
+        //tasksArray[i].color = "#bFFFbF";
+
+        if (colors.indexOf(tasksArray[i].color) === colors.length - 1) {
+          tasksArray[i].color = colors[0];
+        } else {
+          tasksArray[i].color = colors[colors.indexOf(tasksArray[i].color) + 1];
+        }
+      }
+    }
+
+    removeAllChildNodes(divTasks);
+    tasksArray.forEach((e) => renderTasks(e));
+
+    console.log(e);
+  });
 }
-// Deletes from notesArr by ID
+// Deletes from array by ID
 function deleteNote(id) {
   let result = tasksArray.filter((element) => element.id != id);
   tasksArray = result;
-  
 }
-// If there are tasks in LS, render them
+// *****************************
+// Tasklist
+let tasksArray = [];
+// DOM HTML selectors
+const btnSubmit = document.getElementById("btn-submit");
+const divTasks = document.getElementById("div-tasks");
+const inputTask = document.getElementById("input-task");
+const formTask = document.getElementById("form-task");
+// Form listener
+formTask.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (inputTask.value.length > 1000) {
+    alert("Překročili jste limit 1 000 znaků");
+  } else {
+    let taskValue = inputTask.value;
+    let taskColor = "#ffffff";
+    let taskId = uuidv4();
+    tasksArray.push({ id: taskId, task: taskValue, color: taskColor });
+    // Pin to our board
+    renderTasks({ id: taskId, task: taskValue, color: taskColor });
+    // Clear the input
+    inputTask.value = "";
+    // Pass notes to LocalStorage
+    localStorage.setItem("storage", JSON.stringify(tasksArray));
+  }
+});
+
+// After load - if there are tasks in LS, render them
 window.addEventListener("load", function () {
   if (localStorage.getItem("storage") != null) {
     let fromLS = JSON.parse(localStorage.getItem("storage"));
-     
+
     fromLS.forEach((element) => {
       tasksArray.push(element);
-       
+
       renderTasks(element);
     });
-     
   }
 });
-// Masonry init
-function initMasonry() {
-  var container = document.querySelector('#div-tasks');
-  new Masonry(container, {
-    itemSelector: '.col-12',
-    percentPosition: true
-  });
-}
+
 // Onload
-window.addEventListener('load', function() {
+window.addEventListener("load", function () {
   initMasonry();
 });
